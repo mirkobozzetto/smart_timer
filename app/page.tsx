335f2796aea61df0e95fc509bcee7eef14be8557
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-25 20:31:43 +0200

    delete  old code comments

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,8 @@
 import TimePicker from "./components/TimePicker";
 
 export default function Home() {
-  // const [isRunning, setIsRunning] = useState(false);
-  // const startTimer = () => {
-  //   setIsRunning(true);
-  // };
-  // const stopTimer = () => {
-  //   setIsRunning(false);
-  // };
-
   return (
     <main className="flex flex-col justify-between items-center p-24 min-h-screen">
-      {/* <TimePicker
-        startTimer={startTimer}
-        stopTimer={stopTimer}
-        isRunning={isRunning}
-      /> */}
       <TimePicker />
     </main>
   );
