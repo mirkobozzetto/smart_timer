a7e8b65bad304df107cb0fd0387abb9224fb9574
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-27 23:34:50 +0200

    uuid v4 on time button

--- app/components/TimerButtons.tsx
+++ app/components/TimerButtons.tsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import { useTimeStore } from "../store/timeStore";
 
 const TimerButtons = () => {
@@ -5,7 +6,7 @@ const TimerButtons = () => {
     useTimeStore();
 
   const handleCreateTimer = () => {
-    const id = Date.now().toString();
+    const id = uuidv4();
     createTimer(id);
   };
 
